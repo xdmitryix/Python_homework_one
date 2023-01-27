2b2842c4d8e8fd560d5233de90df6b50ff6f2d01
dmitry <rbdeman@yandex.ru>
2023-01-27 22:18:23 +0300

    сделал вариант для целых чисел

--- task_five.py
+++ task_five.py
@@ -1,3 +1,16 @@
 # HARD необязательная, идет за 3 обязательных 
 # Найдите сумму цифр любого вещественного или целого числа. Можно использовать decimal. Через строку решать нельзя.
 
+number = int(input('введи число: '))
+sum = 0
+if number < 0:
+    number = abs(number)
+if number < 10:
+    sum = sum + number
+else:    
+    while number >= 10:
+        sum = sum + number % 10
+        number = number // 10
+    sum = sum + number
+print('сумма цифр равна: ', sum)
+
