c5d6e82a62c1767ce1cfc06ba0efe6d98ec5bea0
dmitry <rbdeman@yandex.ru>
2023-01-29 20:02:36 +0300

    сделал задачу HARD

--- task_five.py
+++ task_five.py
@@ -1,7 +1,17 @@
 # HARD необязательная, идет за 3 обязательных 
 # Найдите сумму цифр любого вещественного или целого числа. Можно использовать decimal. Через строку решать нельзя.
 
-number = int(input('введи число: '))
+from decimal import Decimal
+number = Decimal(input('введи число: '))
+number_float = number
+number_round = round(number) 
+count = 0
+while number_float % 1 != 0:
+    number_float *= 10
+    count += 1
+if count  > 0:
+    for i in range(count):
+        number *= 10
 sum = 0
 if number < 0:
     number = abs(number)
@@ -12,5 +22,4 @@ else:
         sum = sum + number % 10
         number = number // 10
     sum = sum + number
-print('сумма цифр равна: ', sum)
-
+print('сумма цифр равна: ', round(sum))
